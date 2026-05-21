c52805363c2fc3a3bcab9d9a389ed0413704f0ab
Lucas <lucasmanoel.email@gmail.com>
2026-05-21 20:13:23 -0300

    Construção de um novo metodo, autenticação em todos os metodos

--- src/main/java/com/lucasmanoel/habitos/business/HabitosService.java
+++ src/main/java/com/lucasmanoel/habitos/business/HabitosService.java
@@ -8,28 +8,36 @@ import com.lucasmanoel.habitos.infrasctruture.entity.CheckinEntity;
 import com.lucasmanoel.habitos.infrasctruture.entity.HabitosEntity;
 import com.lucasmanoel.habitos.infrasctruture.exceptions.ConflictException;
 import com.lucasmanoel.habitos.infrasctruture.exceptions.ResourceNotFoundException;
+import com.lucasmanoel.habitos.infrasctruture.exceptions.UnauthorizedException;
 import com.lucasmanoel.habitos.infrasctruture.repository.CheckinRepository;
 import com.lucasmanoel.habitos.infrasctruture.repository.HabitosRepository;
 import com.lucasmanoel.habitos.infrasctruture.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class HabitosService {
+    private final JwtUtil jwtUtil;
     private  final HabitosConverter habitosConverter;
     private  final HabitosUpdateConverter habitosUpdateConverter;
     private  final HabitosRepository habitosRepository;
     private  final CheckinRepository checkinRepository;
 
-    public habitosDTORecords cadastroHabito(habitosDTORecords dto){
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+    public habitosDTORecords cadastroHabito(String token, habitosDTORecords dto){
+        String email = jwtUtil.extrairEmailToken(token.substring(7));
         HabitosEntity entity = HabitosEntity.builder()
                 .nome(dto.nome())
                 .descricao(dto.descricao())
@@ -40,29 +48,60 @@ public class HabitosService {
 
         return habitosConverter.paraHabitosDTO(habitosRepository.save(entity));
     }
+    public void alterarStatusHabito(@RequestHeader("Authorization") String token, @PathVariable String id, @RequestParam boolean ativo){
+        if (ativo){
+            ativaHabito(token, id);
+        }else {
+            desativaHabito(token, id);
+        }
+    }
 
-    public void desativaHabito(String id){
+    public void desativaHabito(String token,String id){
+
         HabitosEntity entity = habitosRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Id não encontrado")
         );
+        if (!jwtUtil.extrairEmailToken(token.substring(7)).equals(entity.getEmail())){
+            throw new UnauthorizedException("Usuario não autenticado");
+        }
         if(!entity.getAtivo()){
             throw new ConflictException("Hábito já desativado");
         }
         entity.setAtivo(false);
         habitosRepository.save(entity);
     }
-
-    public void deletaHabito(String id){
-        if (!habitosRepository.existsById(id)){
-            throw new ResourceNotFoundException("Id não encontrado");
-        }
-        habitosRepository.deleteById(id);
-    }
-
-    public habitosDTORecords alteraHabito(habitosDTORecords dto, String id){
+    public void ativaHabito(String token,String id){
         HabitosEntity entity = habitosRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Id não encontrado")
         );
+        if (!jwtUtil.extrairEmailToken(token.substring(7)).equals(entity.getEmail())){
+            throw new UnauthorizedException("Usuario não autenticado");
+        }
+        if(entity.getAtivo()){
+            throw new ConflictException("Hábito já ativado");
+        }
+        entity.setAtivo(true);
+        habitosRepository.save(entity);
+    }
+
+    public void deletaHabito(String token, String id){
+        HabitosEntity entity = habitosRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Id não encontrado")
+        );
+        if (!jwtUtil.extrairEmailToken(token.substring(7)).equals(entity.getEmail())){
+            throw new UnauthorizedException("Usuario não autenticado");
+        }
+        habitosRepository.deleteById(id);
+
+    }
+
+    public habitosDTORecords alteraHabito(String token, habitosDTORecords dto, String id){
+        HabitosEntity entity = habitosRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Id não encontrado")
+        );
+        if (!jwtUtil.extrairEmailToken(token.substring(7)).equals(entity.getEmail())){
+            throw new UnauthorizedException("Usuario não autenticado");
+        }
         if (!entity.getAtivo()){
             throw new ConflictException("Hábito inativo");
         }
@@ -70,10 +109,13 @@ public class HabitosService {
         return habitosConverter.paraHabitosDTO(habitosRepository.save(entity));
     }
 
-    public int efetuarCheckin(String id){
+    public int efetuarCheckin(String token, String id){
         HabitosEntity entity = habitosRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Id não encontrado")
         );
+        if (!jwtUtil.extrairEmailToken(token.substring(7)).equals(entity.getEmail())){
+            throw new UnauthorizedException("Usuario não autenticado");
+        }
         if (!entity.getAtivo()){
             throw new ConflictException("Hábito desativado!");
         }
@@ -85,22 +127,31 @@ public class HabitosService {
                 .data(LocalDate.now())
                 .build();
         checkinRepository.save(checkin);
-        return calcularStreak(entity.getHabitosID());
+        return calcularStreak(token ,entity.getHabitosID());
     }
 
-    public List<CheckinDTORecords> historicoCheckin(String habitoId){
-        if (!habitosRepository.existsById(habitoId)){
-            throw new ResourceNotFoundException("Id do hábito não encontrado");
+    public List<CheckinDTORecords> historicoCheckin(String token, String habitoId){
+        HabitosEntity entity = habitosRepository.findById(habitoId).orElseThrow(
+                () -> new ResourceNotFoundException("Id não encontrado")
+        );
+        if (!jwtUtil.extrairEmailToken(token.substring(7)).equals(entity.getEmail())){
+            throw new UnauthorizedException("Usuario não autenticado");
         }
         LocalDate data = LocalDate.now().minusDays(30);
         List<CheckinEntity> checkins = checkinRepository.findByHabitosIDAndDataAfter(habitoId, data);
         if (checkins.stream().map(c -> new CheckinDTORecords(c.getData())).toList().isEmpty()){
-            throw new ConflictException("Você ainda não fez check-in nesse hábito");
+            throw new ResourceNotFoundException("Você ainda não fez check-in nesse hábito");
         }
         return checkins.stream().map(c -> new CheckinDTORecords(c.getData())).toList();
    }
 
-    public int calcularStreak(String habitoId){
+    public int calcularStreak(String token, String habitoId){
+        HabitosEntity entity = habitosRepository.findById(habitoId).orElseThrow(
+                () -> new ResourceNotFoundException("Id não encontrado")
+        );
+        if (!jwtUtil.extrairEmailToken(token.substring(7)).equals(entity.getEmail())){
+            throw new UnauthorizedException("Usuario não autenticado");
+        }
         List<CheckinEntity> buscarLista = checkinRepository.findByHabitosID(habitoId);
         buscarLista.sort(Comparator.comparing(CheckinEntity::getData).reversed());
 
